4924cc503dda46c1f753995b79491c4e3b3f1efc
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-04-21 15:15:34 +0200

    development

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 function lightTopic() {
-  document.getElementById("personalData").style.color = "black";
-  document.getElementById("fingerlines").style.animationPlayState = "none";
+  document.getElementById("personalData").style.color = "white";
+  document.getElementById("first").style.animationPlayState = "none";
   document.getElementById("first").style.color = "white"; 
 }
 
