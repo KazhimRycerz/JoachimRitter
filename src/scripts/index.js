722cba11d7b129a7814ab1bd92a8a0d162c3174f
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-04-23 20:35:01 +0200

    development

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,14 @@
-function lightTopic() {
+function lightTopicOn() {
   document.getElementById("personalData").style.color = "white";
   document.getElementById("first").style.animationPlayState = "none";
   document.getElementById("first").style.color = "white"; 
 }
 
+function lightTopicOff() {
+  document.getElementById("personalData").style.color = "white";
+  document.getElementById("first").style.animationPlayState = "none";
+  document.getElementById("first").style.color = "white"; 
+}
    /*  document.getElementById("personalData").onmouseover = function () {
     this.style.color = "white";
     document.getElementById("first").style.color = "white"; 
