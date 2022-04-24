9569a63b420afea7cc6e6fcfcb9cb89c4502f0db
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-04-24 13:07:58 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,31 @@
 function lightTopicOn() {
-  document.getElementById("personalData").style.color = "white";
+  document.getElementsByClassName("personalData").style.color = "white";
+  document.getElementById("first").style.animationPlayState = "paused";
+  document.getElementById("second").style.animationPlayState = "none";
+  document.getElementById("third").style.animationPlayState = "none";
+  document.getElementById("fourth").style.animationPlayState = "none";
+  document.getElementById("fifth").style.animationPlayState = "none";
+  document.getElementById("sixth").style.animationPlayState = "none";
+  document.getElementById("seventh").style.animationPlayState = "none";
+  document.getElementById("eigeht").style.animationPlayState = "none";
+  document.getElementById("ninth").style.animationPlayState = "none";
   document.getElementById("first").style.animationPlayState = "none";
-  document.getElementById("first").style.color = "white"; 
+  document.getElementById("first").style.opacity = "1";
 }
 
 function lightTopicOff() {
-  document.getElementById("personalData").style.color = "white";
-  document.getElementById("first").style.animationPlayState = "none";
-  document.getElementById("first").style.color = "white"; 
+  document.getElementsByClassName("personalData").style.color = "black";
+  document.getElementById("first").style.animationPlayState = "running";
+  
 }
    /*  document.getElementById("personalData").onmouseover = function () {
     this.style.color = "white";
+    document.getElementById("first").style.opacity = "1"; 
+    document.getElementById("first").style.animationPlayState = "none";
+  } 
+  
+  document.getElementById("personalData").onmouseout = function () {
+    this.style.color = "black";
     document.getElementById("first").style.color = "white"; 
     document.getElementById("first").style.animationPlayState = "none";
-  } */
+  }*/
