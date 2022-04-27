7729b63464bb32a5f4411249cfc5e573daaa9479
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-04-27 16:26:06 +0200

    bugfix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,32 @@
 function lightTopicOn() {
-  document.getElementsByClassName("personalData").style.color = "white";
+  document.getElementById("personalData").style.color = "white";
   document.getElementById("first").style.animationPlayState = "paused";
+  document.getElementById("first").style.opacity = "1";
   document.getElementById("second").style.animationPlayState = "paused";
+  document.getElementById("second").style.opacity = "0";
   document.getElementById("third").style.animationPlayState = "paused";
   document.getElementById("fourth").style.animationPlayState = "paused";
   document.getElementById("fifth").style.animationPlayState = "paused";
   document.getElementById("sixth").style.animationPlayState = "paused";
   document.getElementById("seventh").style.animationPlayState = "paused";
-  document.getElementById("eigeht").style.animationPlayState = "paused";
+  document.getElementById("eigth").style.animationPlayState = "paused";
   document.getElementById("ninth").style.animationPlayState = "paused";
-  document.getElementById("first").style.animationPlayState = "paused";
+  
  /*  document.getElementById("first").style.opacity = "1"; */
 }
 
 function lightTopicOff() {
-  document.getElementsByClassName("personalData").style.color = "black";
+  document.getElementById("personalData").style.color = "black";
   document.getElementById("first").style.animationPlayState = "running";
+  document.getElementById("second").style.animationPlayState = "running";
+  document.getElementById("third").style.animationPlayState = "running";
+  document.getElementById("fourth").style.animationPlayState = "running";
+  document.getElementById("fifth").style.animationPlayState = "running";
+  document.getElementById("sixth").style.animationPlayState = "running";
+  document.getElementById("seventh").style.animationPlayState = "running";
+  document.getElementById("eigth").style.animationPlayState = "running";
+  document.getElementById("ninth").style.animationPlayState = "running";
+ 
   
 }
    /*  document.getElementById("personalData").onmouseover = function () {
